405042e064a5d5ebf48ff01cd7bc6870e13d92fd
ShubXcode <118995345+ShubXcode@users.noreply.github.com>
2024-10-14 01:19:41 +0530

    initial commit

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import '../Global.css'
 
 const Home = () => {
   return (
     <>
-     <div> THIS IS HOME</div>
+      <section className='homeSection'>
+        <div className="container">
+          <div className="homeHead">
+            <div className="homeHeading">Buy Medicines and Essentials</div>
+            <div className="homeInputFeald"><input type="text" placeholder='Search Medicines' /><lord-icon class="lord-icon" src="https://cdn.lordicon.com/kkvxgpti.json" trigger="hover">
+            </lord-icon></div>
+          </div>
+        </div>
+      </section>
     </>
   )
 }
